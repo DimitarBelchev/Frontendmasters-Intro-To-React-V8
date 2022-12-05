070f4ae627c89917b43e5d0b2f7bcdd33cc51423
DimitarBelchev <52967149+DimitarBelchev@users.noreply.github.com>
2022-12-05 23:46:12 +0200

    Update App.jsx

--- 07-component-composition/src/App.jsx
+++ 07-component-composition/src/App.jsx
@@ -3,12 +3,10 @@ import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <React.StrictMode>
     <div>
       <h1>Adopt Me!</h1>
       <SearchParams />
     </div>
-    <React.StrictMode>
   );
 };
 
