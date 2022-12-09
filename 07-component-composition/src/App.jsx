4fa6ea33bc541df8c1e2b04da4fce9fd5123a945
DimitarBelchev <52967149+DimitarBelchev@users.noreply.github.com>
2022-12-09 23:25:04 +0200

    Update App.jsx

--- 07-component-composition/src/App.jsx
+++ 07-component-composition/src/App.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
+import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <StrictMode>
+    <React.StrictMode>
+      <div>
         <h1>Adopt Me!</h1>
         <SearchParams />
-      </StrictMode>
-    </div>
+      </div>
+    </React.StrictMode>
   );
 };
 
